2bd5e6878bcb3145b0537d239845568b06217e8b
[01;31m[K"name"[m[K: "Cate <[01;31m[K"email"[m[K: null>
2017-11-01 16:06:14 -0500

    initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user_array = 10.times.collect do User.create!(username: Faker::HarryPotter.character, email: Faker::Internet.safe_email, pw_hash: "pass")
+end
+category_array = [Category.create!( name: "Appetizer" ), Category.create!( name: "Salad" ), Category.create!( name: "Main Course" ), Category.create!( name: "Dessert" )]
+
+recipe_array = 20.times.collect do Recipe.create!( name: Faker::Food.dish, difficulty: [1, 2, 3].sample, prep_time: [100, 25, 30].sample, ingredients: "#{Faker::Food.ingredient} #{Faker::Food.measurement}", directions: Faker::Lorem.paragraph, user_id: user_array.sample.id, category_id: category_array.sample.id) end
+
+
+
+
