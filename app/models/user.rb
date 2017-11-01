ddae6ad960bfbe49405e6c8f54429aa3ea69d487
[01;31m[K"name"[m[K: "Cate <[01;31m[K"email"[m[K: null>
2017-11-01 16:36:46 -0500

    update gems, add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ApplicationRecord
   has_many :recipes
 
+  validates :username, :email, :pw_hash, presence: true
+  validates :email, :username, uniqueness: true
+
+
+  def password
+    @password ||= BCrypt::Password.new(pw_hash)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.pw_hash = @password
+  end
+
 end
