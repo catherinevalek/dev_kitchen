82668932c5cc648933df541bc9bf597ca3847626
Lina Zoo <linazooper@gmail.com>
2017-11-01 16:36:00 -0500

    categories controllers, models and views started

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,3 +2,9 @@ class Category < ApplicationRecord
   has_many :recipes
 
 end
+
+# A user should be able to submit a SINGLE recipe to a category 
+# * Appetizer
+# * Salad
+# * Main Course
+# * Dessert
