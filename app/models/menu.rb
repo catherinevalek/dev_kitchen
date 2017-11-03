e2c70a8183e33fac342b220f104b1704c611f995
Cate <email": null>
2017-11-03 14:49:28 -0500

    update form date field

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,5 +1,25 @@
 class Menu < ApplicationRecord
 
   validates :appetizer, :salad, :main_course, :dessert, presence: true
+  has_one :appetizer, foreign_key: :appetizer_id, class_name: "Recipe"
+  has_one :salad, foreign_key: :salad_id, class_name: "Recipe"
+  has_one :main_course, foreign_key: :main_course_id, class_name: "Recipe"
+  has_one :dessert, foreign_key: :dessert_id, class_name: "Recipe"
+  # menu.appetizer = Recipe.find(params[:appetizer_id])
 
+  def self.appetizers
+    Recipe.is_appetizer
+  end
+
+  def self.salads
+    Recipe.is_salad
+  end
+
+  def self.main_courses
+    Recipe.is_main_course
+  end
+
+  def self.desserts
+    Recipe.is_dessert
+  end
 end
--- db/migrate/20171103131814_create_menus.rb
+++ db/migrate/20171103131814_create_menus.rb
@@ -1,10 +1,10 @@
 class CreateMenus < ActiveRecord::Migration[5.1]
   def change
     create_table :menus do |t|
-      t.integer :appetizer, null: false
-      t.integer :salad, null: false
-      t.integer :main_course, null: false
-      t.integer :dessert, null: false
+      t.integer :appetizer_id, null: false
+      t.integer :salad_id, null: false
+      t.integer :main_course_id, null: false
+      t.integer :dessert_id, null: false
       t.date :date, null: false
 
       t.timestamps
