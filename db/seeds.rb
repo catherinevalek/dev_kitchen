5b7f2fde016873293bcb583c93735099c7ef8418
Catherine Valek <catherine.valek@gmail.com>
2017-11-05 12:51:02 -0600

    add menu seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,8 @@
-test_password = "pass"
-
-test_pw_hash = BCrypt::Password.create(test_password)
-
 user_array = 10.times.collect do User.create!(username: Faker::HarryPotter.character, email: Faker::Internet.safe_email, password: "pass")
 end
 category_array = [Category.create!( name: "Appetizer" ), Category.create!( name: "Salad" ), Category.create!( name: "Main Course" ), Category.create!( name: "Dessert" )]
 
 recipe_array = 20.times.collect do Recipe.create!( name: Faker::Food.dish, difficulty: [1, 2, 3].sample, prep_time: [100, 25, 30].sample, ingredients: "#{Faker::Food.ingredient} #{Faker::Food.measurement}", directions: Faker::Lorem.paragraph, user_id: user_array.sample.id, category_id: category_array.sample.id) end
+
+
+menu = Menu.create!(appetizer_id: 13, salad_id: 11, main_course_id: 4, dessert_id: 10, date: "2017-11-05")
